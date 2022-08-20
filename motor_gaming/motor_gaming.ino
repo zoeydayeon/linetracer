e2ac1b05fad53841e8f42614d0b506575448c058
Dayeon Lee <christylee@Zoey-Dayeon-Lee.local>
2022-08-20 19:50:36 +0900

    changed photo...

--- motor_gaming/motor_gaming.ino
+++ motor_gaming/motor_gaming.ino
@@ -4,15 +4,15 @@
 AF_DCMotor motor1(1);
 AF_DCMotor motor4(4);
 
-int sensor_L = A0;
+int sensor_L = A1;
 int sensor_R = A5;
 
 
 void setup() {
-  motor1.setSpeed(175);
+  motor1.setSpeed(230);
   motor1.run(RELEASE);
   
-  motor4.setSpeed(200);
+  motor4.setSpeed(230);
   motor4.run(RELEASE);
 
   Serial.begin(9600);
